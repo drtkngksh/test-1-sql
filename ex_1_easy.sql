39826fd993cd20338817d4543d7d2e0a742b9d28
ignatievnick <ignatievnick1996@gmail.com>
2023-05-21 17:10:01 +0300

    ex_1_easy.sql
    добавлены комментарии к коду

--- ex_1_easy.sql
+++ ex_1_easy.sql
@@ -1,22 +1,22 @@
 SELECT
-  TO_CHAR(TO_DATE(transactions.transaction_date, 'DD-MM-YYYY'), 'MM-YYYY') AS date,
-  customers.wealth_segment AS wealth_segment,
+  TO_CHAR(TO_DATE(transactions.transaction_date, 'DD-MM-YYYY'), 'MM-YYYY') AS date, -- перевод даты в ММ-УУУУ формат
+  customers.wealth_segment AS wealth_segment, -- сегмент пользователя
   SUM(CAST(REPLACE(
     REPLACE(
-      REPLACE(transactions.standard_cost, '[$]', ''),
+      REPLACE(transactions.standard_cost, '[$]', ''), -- убрать пробелы и знак доллара 
       ',',
       '.'
     ),
     ' ',
     ''
-  ) AS float)),
-  COUNT(DISTINCT(transactions.customer_id))
+  ) AS float)) AS cost, -- оборот пользователя
+  COUNT(DISTINCT(transactions.customer_id)) AS MAU -- подсчет MAU
 FROM public.transaction_test_nick AS transactions
-  INNER JOIN public.customers_test_nick AS customers
+  INNER JOIN public.customers_test_nick AS customers -- объединение таблицы транзакций и клиентов
     ON transactions.customer_id = customers.customer_id
 WHERE(
   transactions.standard_cost <> ''
-  AND transactions.standard_cost IS NOT NULL) 
-  AND transactions.order_status = 'Approved' 
-GROUP BY date, wealth_segment
-ORDER BY date, wealth_segment
+  AND transactions.standard_cost IS NOT NULL) -- отфильтровать нулевые и пустые значения 
+  AND transactions.order_status = 'Approved' -- отфильтровать неутвержденные транзакции
+GROUP BY date, wealth_segment -- группировка по дате и сегменту
+ORDER BY date, wealth_segment -- сортировка по дате и сегменту
