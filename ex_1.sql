103db23cb0251601a1042e276e254caa6bfb3326
ignatievnick <ignatievnick1996@gmail.com>
2023-05-21 16:58:12 +0300

    ex_1_comments.sql
    добавил комментарии в код

--- ex_1.sql
+++ ex_1.sql
@@ -1,23 +1,23 @@
 SELECT
-  TO_CHAR(TO_DATE(transactions.transaction_date, 'DD-MM-YYYY'), 'MM-YYYY') AS date,
-  customers.wealth_segment AS wealth_segment,
+  TO_CHAR(TO_DATE(transactions.transaction_date, 'DD-MM-YYYY'), 'MM-YYYY') AS date, -- перевод даты в ММ-УУУУ формат
+  customers.wealth_segment AS wealth_segment, -- сегмент пользователя
   ROUND(SUM(CAST(REPLACE(
     REPLACE(
-      REPLACE(transactions.standard_cost, '[$]', ''),
+      REPLACE(transactions.standard_cost, '[$]', ''), -- убрать пробелы и знак доллара
       ',',
       '.'
     ),
     ' ',
     ''
-  ) AS float))) AS cost,
-  COUNT(DISTINCT(transactions.customer_id)) AS MAU
-FROM public.transaction_test_nick AS transactions
-  INNER JOIN public.customers_test_nick AS customers
+  ) AS float))) AS cost, -- оборот пользователя
+  COUNT(DISTINCT(transactions.customer_id)) AS MAU -- подсчет MAU
+FROM public.transaction_test_nick AS transactions 
+  INNER JOIN public.customers_test_nick AS customers -- объединение таблицы транзакций и клиентов
     ON transactions.customer_id = customers.customer_id
 WHERE(
   transactions.standard_cost <> ''
-  AND transactions.standard_cost IS NOT NULL) 
-  AND transactions.order_status = 'Approved' 
+  AND transactions.standard_cost IS NOT NULL) -- отфильтровать нулевые и пустые значения 
+  AND transactions.order_status = 'Approved' -- отфильтровать неутвержденные транзакции
   
   AND transactions.customer_id IN (
 SELECT
@@ -25,6 +25,6 @@ SELECT
 FROM public.transaction_test_nick transactions
 GROUP BY 1
 HAVING MAX(EXTRACT(MONTH FROM to_date(transaction_date, 'DD-MM-YYYY'))) = 12 AND (MAX(EXTRACT(MONTH FROM to_date(transaction_date, 'DD-MM-YYYY'))) - MIN(EXTRACT(MONTH FROM to_date(transaction_date, 'DD-MM-YYYY'))) + 1) = COUNT(DISTINCT((EXTRACT(MONTH FROM to_date(transaction_date, 'DD-MM-YYYY')))))
-)
-GROUP BY date, wealth_segment
-ORDER BY date, wealth_segment
+) -- отсеивать клиентов, которые не совершали покупки в каждом месяце года
+GROUP BY date, wealth_segment -- группировка по дате и сегменту
+ORDER BY date, wealth_segment -- сортировка по дате и сегменту
